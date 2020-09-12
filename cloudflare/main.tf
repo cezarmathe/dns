43e0ab7813323852767c14b62efbb5dabe00fbcc
cezarmathe <me@cezarmathe.com>
2020-09-12 12:58:00 +0300

    Change cloudflare provider

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -9,7 +9,8 @@ terraform {
   }
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
     }
   }
   required_version = ">= 0.13"
