ac816b50f62f258df2c2059e6f6854a61dbd091d
cezarmathe <me@cezarmathe.com>
2020-09-12 12:40:45 +0300

    Attempt to fix record data issue

--- cloudflare/mail.tf
+++ cloudflare/mail.tf
@@ -9,28 +9,11 @@ resource "cloudflare_record" "box_cezarmathe_com" {
   proxied = false
 }
 
-resource "cloudflare_record" "mx_cezarmathe_com" {
-  zone_id = cloudflare_zone.main.id
-  name    = "cezarmathe.com"
-  type    = "MX"
-  ttl     = 1
-  proxied = false
-
-  data    = {
-    weight = 10
-    target = "box.cezarmathe.com"
-  }
-}
-
 resource "cloudflare_record" "mx_box_cezarmathe_com" {
   zone_id = cloudflare_zone.main.id
   name    = "box.cezarmathe.com"
+  value   = "10 box.cezarmathe.com"
   type    = "MX"
   ttl     = 1
   proxied = false
-
-  data    = {
-    weight = 10
-    target = "box.cezarmathe.com"
-  }
 }
