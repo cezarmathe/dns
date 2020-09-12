18b03168f3cd10d16582fbb879d7288d78b1111f
cezarmathe <me@cezarmathe.com>
2020-09-12 12:43:47 +0300

    Attempt to fix record data issue

--- cloudflare/mail.tf
+++ cloudflare/mail.tf
@@ -12,7 +12,7 @@ resource "cloudflare_record" "box_cezarmathe_com" {
 resource "cloudflare_record" "mx_box_cezarmathe_com" {
   zone_id = cloudflare_zone.main.id
   name    = "box.cezarmathe.com"
-  value   = "10 box.cezarmathe.com"
+  value   = "box.cezarmathe.com"
   type    = "MX"
   ttl     = 1
   proxied = false
